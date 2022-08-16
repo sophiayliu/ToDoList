86ed37a03d589ddf0ac6eca39ed366ea72940ac9
Sophia Liu <sophia.ch102@gmail.com>
2022-08-16 09:32:21 -0400

    Iteration 0

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Sophia Liu on 8/16/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ ToDoList/ToDoListViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ToDoListViewController.swift
+//  ToDoList
+//
+//  Created by Sophia Liu on 8/16/22.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    var toDos : [ToDo] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        toDos = createToDos()
+    }
+    
+    func createToDos() -> [ToDo] {
+        
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the dog"
+        // important is set to false by default
+        
+        return [swift, dog]
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+        
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+        
+        return cell
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
